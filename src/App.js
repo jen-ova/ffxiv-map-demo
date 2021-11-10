700ea4620bd9379a53dfcb27c58278066d2a11b0
Jen Stewart <jenstewart2404@gmail.com>
2021-11-10 18:08:03 +0000

    basic leaflet map setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MapContainer, ImageOverlay, Marker, Popup } from "react-leaflet";
+import { CRS } from "leaflet";
+import arcanistmiddlelanoscea from "./data/arcanist-middlelanoscea.json";
 
 function App() {
+  console.log(arcanistmiddlelanoscea);
+  const toLatLong = ([x, y]) => [42 - y, x];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer
+        bounds={[
+          [1, 1],
+          [42, 42],
+        ]}
+        maxBounds={[
+          [1, 1],
+          [42, 42],
+        ]}
+        attributionControl={false}
+        crs={CRS.Simple}
+        zoomSnap={0}>
+        <ImageOverlay
+          bounds={[
+            [1, 1],
+            [42, 42],
+          ]}
+          url="https://xivapi.com/m/s1f1/s1f1.00.jpg"
+        />
+
+        {arcanistmiddlelanoscea.map((arc) => (
+          <Marker key={arc.id} position={toLatLong([arc.x, arc.y])}>
+            <Popup>
+              <img src={arc.mobIcon} alt={arc.mobName} />
+              <p>{arc.mobName}</p>
+              <p>
+                X: {arc.x}, Y: {arc.y}
+              </p>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
